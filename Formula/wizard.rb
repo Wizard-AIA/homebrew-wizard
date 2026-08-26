a4800612e432f600f040362f5d5ccddb826a5558
Aniket Saha <141211011+Aniket-a14@users.noreply.github.com>
2026-08-26 20:17:20 +0530

    release: update Homebrew formula to v1.0.2

--- Formula/wizard.rb
+++ Formula/wizard.rb
@@ -1,26 +1,26 @@
 class Wizard < Formula
   desc "Local-first autonomous data analysis agent"
   homepage "https://wizard-aia.github.io/docs/"
-  version "1.0.1"
+  version "1.0.2"
   license "BSD-3-Clause"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Wizard-AIA/Wizard-w2/releases/download/v1.0.1/Wizard-v1.0.1-darwin-arm64.zip"
-      sha256 "2a205b76f815eeefa1aca7d23493b6ac913f5bc15cf883e1aba58ee6e2a5fb26"
+      url "https://github.com/Wizard-AIA/Wizard-w2/releases/download/v1.0.2/Wizard-v1.0.2-darwin-arm64.zip"
+      sha256 "cb254f5c6a3acdb34bee51b906d3f2144d26cd5fdc9658ce11586c065e5b8a04"
     else
-      url "https://github.com/Wizard-AIA/Wizard-w2/releases/download/v1.0.1/Wizard-v1.0.1-darwin-amd64.zip"
-      sha256 "90c4598110a6cfdde5d4eb65fbbe065cc855f1d39b11c0b900acb4d87f3bd090"
+      url "https://github.com/Wizard-AIA/Wizard-w2/releases/download/v1.0.2/Wizard-v1.0.2-darwin-amd64.zip"
+      sha256 "c9bb20454e831cf579bf03b7508b676d1e8aa67f922cc4293a6828a22a608fb5"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/Wizard-AIA/Wizard-w2/releases/download/v1.0.1/Wizard-v1.0.1-linux-arm64.zip"
-      sha256 "8ff3be553eeef46809b6d520cab2fd3f8751dd2c0045db436a5c7322bd54ff59"
+      url "https://github.com/Wizard-AIA/Wizard-w2/releases/download/v1.0.2/Wizard-v1.0.2-linux-arm64.zip"
+      sha256 "089c486c92f8b938e3e12fb73a858b7947ab0b6dd80b604f3444913a0c8beb9f"
     else
-      url "https://github.com/Wizard-AIA/Wizard-w2/releases/download/v1.0.1/Wizard-v1.0.1-linux-amd64.zip"
-      sha256 "444a00f199733e488edf29d05bfbe2bda8376f8dc2eb42068ebacac430ebd57b"
+      url "https://github.com/Wizard-AIA/Wizard-w2/releases/download/v1.0.2/Wizard-v1.0.2-linux-amd64.zip"
+      sha256 "ab8ac2cb61e880a83b3d239a265a210eb60a090dd0192d85611fa47bb43543c3"
     end
   end
 
