f990392ab51f4f03298635f0fd507f7a6d24b5c5
Aniket-a14 <aniketsahaworkspace@gmail.com>
2026-08-28 16:19:01 +0530

    chore(release): update formula for v1.0.5 release

--- Formula/wizard.rb
+++ Formula/wizard.rb
@@ -5,21 +5,21 @@ class Wizard < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Wizard-AIA/Wizard-w2/releases/download/v1.0.4/Wizard-v1.0.4-darwin-arm64.zip"
-      sha256 "61799090cc854d6d66052c4403445ed46a3d5facbf434ae2f59c86ce5c1a93f3"
+      url "https://github.com/Wizard-AIA/Wizard-w2/releases/download/v1.0.5/Wizard-v1.0.5-darwin-arm64.zip"
+      sha256 "c5345e1e6d180b0d41f11c0ae5538dfe346a539cf27e1658070be7ae3169ee24"
     else
-      url "https://github.com/Wizard-AIA/Wizard-w2/releases/download/v1.0.4/Wizard-v1.0.4-darwin-amd64.zip"
-      sha256 "ca9395d10b49f136df138dcf8a11f4f04641b57dd6e2c4a0ca139d211b1222b1"
+      url "https://github.com/Wizard-AIA/Wizard-w2/releases/download/v1.0.5/Wizard-v1.0.5-darwin-amd64.zip"
+      sha256 "4087d941deadcadfaafec05930fb9b591410867d24e2b91059e72df740a1de7b"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/Wizard-AIA/Wizard-w2/releases/download/v1.0.4/Wizard-v1.0.4-linux-arm64.zip"
-      sha256 "356891ffe6c3ec9438f2ba8f23ffd1f1f33ee9053c4735517c68ea11439e74d1"
+      url "https://github.com/Wizard-AIA/Wizard-w2/releases/download/v1.0.5/Wizard-v1.0.5-linux-arm64.zip"
+      sha256 "6199e900c8a771ff87af9be199a2c2d1ba730c06a506a79bf1c1eb986b93017f"
     else
-      url "https://github.com/Wizard-AIA/Wizard-w2/releases/download/v1.0.4/Wizard-v1.0.4-linux-amd64.zip"
-      sha256 "372e732f266ab052b238383b1f57b9fd8231b7c41e9f850e13c118df1b8175d8"
+      url "https://github.com/Wizard-AIA/Wizard-w2/releases/download/v1.0.5/Wizard-v1.0.5-linux-amd64.zip"
+      sha256 "fb6fa8b7c4b5becc69b710684a24ad5cc60b8c4636203fa2a0cd1e451cb65787"
     end
   end
 
